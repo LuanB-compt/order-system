7e67f97139892dc17dc25068f0db692e18cd0f67
LuanB-compt <luan-bruno-domingues@hotmail.com>
2022-11-15 00:09:10 -0300

    Created another file to Product object

--- src/order.java
+++ src/order.java
@@ -1,21 +1,3 @@
-public class Product{
-  /*DECLARING ATTRIBUTES*/
-  private String product_name;
-  private String description;
-  private float price;
-  private Provider provider;
-
-  /*CONSTRUCTOR METHOD*/
-  public Product(String product_name, String description, float price, Provider provider){
-    this.product_name = product_name;
-    this.description = description;
-    this.price = price;
-    this.provider = provider;
-  }
-}
-
-
-
 public class order{
   /*DECLARING ATTRIBUTES*/
   private Product item;
