be202374acbd22edd713ea1b5dbc139af2f69a5b
LuanB-compt <luan-bruno-domingues@hotmail.com>
2022-11-15 00:24:21 -0300

    Removed the old main system

--- src/system.java
+++ src/system.java
@@ -1,23 +1,11 @@
 /*IMPORT LIBRARYS*/
 import java.util.*;
 
-
+  
 
 /*MAIN FUNCTION*/
 public class system{
   public static void main(String[] args){
-    client[] clientes = new client[3];
-    String[] nomes = {"Luan", "Helena", "Gaigue"};
-    String[] emails = {"ab", "cd", "ef"};
-    boolean[] companys = {false, false, true};
-    String[] docs = {"1.2", "3.4", "5.6"};
 
-    for(int i=0; i<clientes.length; i++){
-      clientes[i] = new client(nomes[i], emails[i], companys[i], docs[i]);
-    }
-
-    for(client i:clientes){
-      System.out.println(i.get_name());
-    }
   }
 }
