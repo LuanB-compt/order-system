24dd880dccffcb89cb954053b31ea7daab095ff8
LuanB-compt <luan-bruno-domingues@hotmail.com>
2022-11-14 23:58:31 -0300

    Added the description to Product object

--- src/order.java
+++ src/order.java
@@ -1,12 +1,16 @@
 public class Product{
   /*DECLARING ATTRIBUTES*/
   private String product_name;
+  private String description;
   private float price;
+  private Provider provider;
 
   /*CONSTRUCTOR METHOD*/
-  public Product(String product_name, float price){
+  public Product(String product_name, String description, float price, Provider provider){
     this.product_name = product_name;
+    this.description = description;
     this.price = price;
+    this.provider = provider;
   }
 }
 
