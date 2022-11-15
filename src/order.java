461c5ce458d9d906e2b071f6475ec1f288a75f04
LuanB-compt <luan-bruno-domingues@hotmail.com>
2022-11-15 15:34:20 -0300

    Added the getters and setter to order object

--- src/order.java
+++ src/order.java
@@ -14,4 +14,40 @@ public class order{
     this.id = id;
     this.date = date;
   }
+
+  /*GETTERS*/
+  public Product get_item(){
+    return item;
+  }
+  public int get_item_amount(){
+    return item_amount;
+  }
+  public float get_total_price(){
+    return total_price;
+  }
+  public int get_id(){
+    return id;
+  }
+  public String get_date(){
+    return date;
+  }
+  
+  /*SETTERS*/
+  public void set_item(Product item){
+    this.item = item;
+  }
+  public void set_item_amount(int item_amount){
+    this.item_amount = item_amount;
+  }
+  public void set_total_price(float total_price){
+    this.total_price = total_price;
+  }
+  public void set_id(int id){
+    this.id = id;
+  }
+  public void set_date(String date){
+    this.date = date;
+  }
+
+  /*OTHER METHODS*/
 }
